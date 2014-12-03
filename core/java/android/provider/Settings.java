d0b57d989d3bcea7a6cc6ca545bf79fe405be0c4
Lars Greiss <kufikugel@googlemail.com>
2014-12-03 19:56:41 +0100

    Return: Smart Pulldown [1/2]
    Adds the ability to directly open the QS panel when there are
    no notifications present in the notification panel.
    This commit combines the enhancement by HardCorePawn for detectin
    notification types for the same.
    
    Credits: kufikugel, HardCorePawn
    
    PS 2-4 : Cleanup
    
    Adapted to LP
    
    Added Jubakubas commit
    
    SystemUI: Smart Pull-Down - Any Notification (1/2)
    
    PS
    - fixed logic to go inline with all mode in settings - should be fixed on kk as well
    
    Change-Id: I3c9b048f2616d2b8950930cc5bfac27c2364e826
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
    
    Fix Smart Pulldown
    
    Currently if No dismissible or persistent is chosen, quick setting still appears when persistant notifications are present.  This fixes that.
    
    Change-Id: Id8907a4f888e48ed511dd11b484508eca93fa0b3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3502,6 +3502,13 @@ public final class Settings {
         public static final String LISTVIEW_INTERPOLATOR = "listview_interpolator";
 
         /**
+         * Quick Settings Smart Pulldown
+         *
+         * @hide
+         */
+        public static final String QS_SMART_PULLDOWN = "qs_smart_pulldown";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
