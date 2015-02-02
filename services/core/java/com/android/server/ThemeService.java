b320030d07f13e601c7441dd0005ff2a77d5e528
Clark Scheff <clark@cyngn.com>
2015-02-02 09:42:23 -0800

    Themes: Add tm command line tool
    This will allow us to perform automated testing using the theme
    manager as well as a quick way to perform the various functions
    that the theme service provides via the command line.
    
    Change-Id: Id0aa906bb3400225b93080f7a8c2284a8fcf3c44

--- services/core/java/com/android/server/ThemeService.java
+++ services/core/java/com/android/server/ThemeService.java
@@ -125,6 +125,7 @@ public class ThemeService extends IThemeService.Stub {
     private class ThemeWorkerHandler extends Handler {
         private static final int MESSAGE_CHANGE_THEME = 1;
         private static final int MESSAGE_APPLY_DEFAULT_THEME = 2;
+        private static final int MESSAGE_REBUILD_RESOURCE_CACHE = 3;
 
         public ThemeWorkerHandler(Looper looper) {
             super(looper);
@@ -139,6 +140,9 @@ public class ThemeService extends IThemeService.Stub {
                     break;
                 case MESSAGE_APPLY_DEFAULT_THEME:
                     doApplyDefaultTheme();
+                    break;
+                case MESSAGE_REBUILD_RESOURCE_CACHE:
+                    doRebuildResourceCache();
                     break;
                 default:
                     Log.w(TAG, "Unknown message " + msg.what);
@@ -466,6 +470,11 @@ public class ThemeService extends IThemeService.Stub {
                 Log.w(TAG, "Unable to set default theme", e);
             }
         }
+    }
+
+    private void doRebuildResourceCache() {
+        FileUtils.deleteContents(new File(ThemeUtils.RESOURCE_CACHE_DIR));
+        processInstalledThemes();
     }
 
     private void updateProvider(Map<String, String> componentMap) {
@@ -1030,6 +1039,13 @@ public class ThemeService extends IThemeService.Stub {
         mContext.enforceCallingOrSelfPermission(
                 Manifest.permission.ACCESS_THEME_MANAGER, null);
         mProcessingListeners.unregister(listener);
+    }
+
+    @Override
+    public void rebuildResourceCache() throws RemoteException {
+        mContext.enforceCallingOrSelfPermission(
+                Manifest.permission.ACCESS_THEME_MANAGER, null);
+        mHandler.sendEmptyMessage(ThemeWorkerHandler.MESSAGE_REBUILD_RESOURCE_CACHE);
     }
 
     private void purgeIconCache() {
